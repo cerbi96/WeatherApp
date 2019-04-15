434119b5cc644562873bd4916387a6a2971aea87
Cerbi96 <lukaszgorecki96@gmail.com>
2019-04-15 17:23:37 +0200

    Axios and collecting JSON data from API

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ import "./sass/app.scss";
 import TopSection from "./components/top/index";
 import BottomSection from "./components/bottom/index";
 
+
+import axios from "axios";
+import Weather from "./components/top/weather";
+
+const WEATHER_KEY = "42a5a9a814ae4e37a80150506191504";
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cityName: "London",
+      forecastDays: 5,
+    }
+  }
+
+  componentDidMount(){
+    const { cityName,forecastDays} = this.state;
+    const URL = `http://api.apixu.com/v1/forecast.json?key=${WEATHER_KEY}&q=${cityName}&days=${forecastDays}`;
+    axios.get(URL).then((res)=>{
+      console.log("DATA: ",res);
+    }).catch((err)=>{
+      if(err)
+      console.error("Cannot fetch Weather Data from API",err);
+    })
+  }
   render() {
     return (
       <div className="app-container">
